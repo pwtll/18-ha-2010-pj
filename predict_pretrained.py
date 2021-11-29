a0e5f2ad2a1adc0082062a37f88a80344597bd7f
pwtll <82121258+pwtll@users.noreply.github.com>
2021-11-29 16:43:52 +0100

    - corrected predict.py method which is working now
    - added data preprocessing of a whole ecg_lead into a single three peak ecg images, because multiple images per ecg_lead create wrong prediction lists
    
    - added boolean variable: "binary_classification" as option for two or four class training and prediction
    
    -   - added boolean variable: "save_trained_model" as option to save the trained model

--- predict_pretrained.py
+++ predict_pretrained.py
@@ -20,11 +20,11 @@ import time
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Predict given Model')
-    parser.add_argument('--test_dir', action='store',type=str,default='../test/')
-    parser.add_argument('--model_name', action='store',type=str,default='model')  # ,default='model.npy')
+    parser.add_argument('--test_dir', action='store',type=str,default='../test_pred/')  # ToDo: reset to: default='../test/'
+    parser.add_argument('--model_name', action='store',type=str,default='dataset/saved_model/custom_model_2d_cnn_v3_four_classes-num_epochs_10-batch_size_32-image_size_128_20211129-154826')  # ,default='model.npy')
     args = parser.parse_args()
     
-    ecg_leads,ecg_labels,fs,ecg_names = load_references(args.test_dir) # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name                                                # Sampling-Frequenz 300 Hz
+    ecg_leads,ecg_labels,fs,ecg_names = load_references(args.test_dir)  # Importiere EKG-Dateien, zugehörige Diagnose, Sampling-Frequenz (Hz) und Name
     
     start_time = time.time()
     predictions = predict_labels(ecg_leads,fs,ecg_names,model_name=args.model_name)
